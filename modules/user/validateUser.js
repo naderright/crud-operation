3f0bff23b93246ba209b7330c6fd8f81c2a0bb51
nader mohamed <nadercs2018@gmail.com>
2024-03-29 11:42:49 +0200

    .

--- modules/user/validateUser.js
+++ modules/user/validateUser.js
@@ -2,15 +2,20 @@ const Joi = require('joi');
 
 const signupValidation = {
     body: Joi.object().required().keys({
-        name: Joi.string().required().messages({
+        first_name: Joi.string().required().messages({
+            'string.base':"sorry name must follow string char only",
+            'string.empty':'plz fill u name',
+            'any.required':'please enter u name'
+        }),
+        last_name: Joi.string().required().messages({
             'string.base':"sorry name must follow string char only",
             'string.empty':'plz fill u name',
             'any.required':'please enter u name'
         }),
         email : Joi.string().email().required(),
         password : Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
-        cPassword: Joi.ref('password'),
-        phone:Joi.number().required()
+        // cPassword: Joi.ref('password'),
+        // phone:Joi.number().required()
     })
 };
 const signinValidation = {
